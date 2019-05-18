ffd211bf1680fca02c2dc27038c2766230fc9caf
RudBob <1014698129@qq.com>
2019-05-18 11:03:55 +0800

    前序+中序----》二叉树

--- src/_006_reConstructBinaryTree.java
+++ src/_006_reConstructBinaryTree.java
@@ -1,4 +1,19 @@
+/**
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * 例如
+ * 输入前序遍历序列{1,2,4,7,3,5,6,8}；
+ * 中序遍历序列{4,7,2,1,5,3,8,6}，
+ * 则重建二叉树并返回。
+ */
 public class _006_reConstructBinaryTree {
+    /**
+     * 前序+中序----》二叉树
+     * 使用前序找到root节点，找到其在中序中的对应位置；
+     * 那么在中序中：root左侧为左子树，右侧为右子数。
+     * 递归重复以上过程即可
+     */
+
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         // 真正有用的不是前序数组，而是数组在前序中出现的次序。
         int[] index = new int[pre.length];
@@ -10,12 +25,13 @@ public class _006_reConstructBinaryTree {
                 }
             }
         }
+        // 初始化完毕，前序已经呗转化为了其对应的中序的位置。
         TreeNode root = reConstructBinaryTree(in, index, 0, index.length - 1);
         return root;
     }
 
     private TreeNode reConstructBinaryTree(int[] in, int[] index, int head, int last) {
-        if(head > last){
+        if (head > last) {
             return null;
         }
         int minIndex = head;
