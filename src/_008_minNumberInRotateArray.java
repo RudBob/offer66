c6b95666d9c13c8c300b03c8272e2e3b8e139296
RudBob <1014698129@qq.com>
2019-05-18 12:05:44 +0800

    旋转数组的最小数字

--- src/_008_minNumberInRotateArray.java
+++ src/_008_minNumberInRotateArray.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
+ * 例如
+ * <p>
+ * 数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，
+ * 该数组的最小值为1。
+ * <p>
+ * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
 public class _008_minNumberInRotateArray {
     // 二分查找的变体
     public int minNumberInRotateArray(int[] nums) {
@@ -9,6 +19,9 @@ public class _008_minNumberInRotateArray {
         return minNumberInRotateArray(nums, 0, nums.length - 1);
     }
 
+    /**
+     * 重点是特殊情况 [1,1,1,0,1],这种情况下，就只能遍历了
+     */
     private int minNumberInRotateArray(int[] nums, int head, int last) {
         if (head == last - 1) {
             return nums[last] < nums[head] ? nums[last] : nums[head];
@@ -29,7 +42,5 @@ public class _008_minNumberInRotateArray {
         } else {
             return minNumberInRotateArray(nums, head, mid);
         }
-        // 此时处以
-
     }
 }
