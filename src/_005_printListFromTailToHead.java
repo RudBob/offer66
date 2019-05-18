b0e0cc0227df88f7d726001ba51fe8ba1e034ae0
RudBob <1014698129@qq.com>
2019-05-18 10:58:40 +0800

    从尾到头打印链表

--- src/_005_printListFromTailToHead.java
+++ src/_005_printListFromTailToHead.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
 
+/**
+ * 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
 public class _005_printListFromTailToHead {
-    // 要么用栈，要么递归.
+    /**
+     * 使用ArrayList当作栈，
+     * 当然也可以直接使用Java提供的Stack
+     * 或者手动得到链表容量后使用一个数组.
+     */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> res = new ArrayList<>();
         // 从头到尾读入后翻转数组.
@@ -15,7 +22,7 @@ public class _005_printListFromTailToHead {
             //swap(head,last)
             int temp = res.get(head);
             res.set(head, res.get(last));
-            res.set(last,temp);
+            res.set(last, temp);
 
             head++;
             last--;
@@ -23,9 +30,18 @@ public class _005_printListFromTailToHead {
         return res;
     }
 
-    // 递归, 虽然很简洁易懂，但是可能会因为Node数目过多而爆掉.
-        //        printListFromTailToHead(listNode, res);
-    // 如果可以使用Java自带的栈最好，不可以也不慌，就用一个ArrayList就够
+    /**
+     * 递归
+     */
+    public ArrayList<Integer> printListFromTailToHead_1(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+        printListFromTailToHead(listNode, res);
+        return res;
+    }
+    /**
+     * 使用递归的方式，但是如果链表过长，那么JVM中的方法栈的容量会过大，可能导致爆掉。
+     */
+    //        printListFromTailToHead(listNode, res);
     private void printListFromTailToHead(ListNode listNode, ArrayList<Integer> res) {
         if (listNode.next != null) {
             printListFromTailToHead(listNode.next, res);
@@ -33,4 +49,13 @@ public class _005_printListFromTailToHead {
         res.add(listNode.val);
     }
 }
-
+/**
+ * class ListNode {
+ * int val;
+ * ListNode next = null;
+ * <p>
+ * ListNode(int val) {
+ * this.val = val;
+ * }
+ * }
+ */
